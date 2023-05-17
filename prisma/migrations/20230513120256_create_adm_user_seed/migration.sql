ac8d57644ee67f2930d59a075e903af4c156fef9
Gabriel Rodrigo Lampe <grlampe@hotmail.com>
2023-05-17 20:47:53 -0300

    chore: feito seed de usuario e script para migration

---
+++ prisma/migrations/20230513120256_create_adm_user_seed/migration.sql
@@ -0,0 +1,3 @@
+INSERT INTO gymapp.tb_user
+(id, "name", email, "password", cpf, "birthDate", address_street, address_number, address_zip_code, address_district, address_city, address_state, address_complement, active, created_at, updated_at)
+VALUES('27f1e478-97ca-471e-b550-27cca10794f3', 'Administrador', 'adm@personalgymapp.com', '$2a$10$QaZC57OZMQ.kVnsnsX3M6uE2LN6doDCFYBXi.EFCm4EiQJapW05rW', '000.000.000-00', '1996-04-06', 'Rua Firman Neto', '216', '86.808-020', 'Vila São José', 'Apucarana', 'PR', NULL, true, '2023-05-13 08:58:52.897', '2023-05-13 08:58:52.897');
