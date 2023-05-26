ea52f506bdaa522522ec30a351cdc1473b3ea6cf
Gabriel Lampe <grlampe@hotmail.com>
2023-05-26 15:26:02 -0300

    chore: ajustado rotas para usar class validator

--- src/modules/user/dtos/createUserDTO.ts
+++ src/modules/user/dtos/createUserDTO.ts
@@ -1,16 +1,63 @@
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateUserDTO {
   readonly id: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly cpf: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   readonly birthDate: Date;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressStreet: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressZipCode: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressDistrict: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressCity: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressState: string;
+
+  @IsOptional()
+  @IsString()
   readonly addressComplement: string;
+
+  @IsOptional()
+  @IsBoolean()
   readonly active: boolean;
 }
--- src/modules/user/dtos/updateUserDTO.ts
+++ src/modules/user/dtos/updateUserDTO.ts
@@ -1,16 +1,65 @@
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class UpdateUserDTO {
+  @IsNotEmpty()
+  @IsString()
   readonly id: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly cpf: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   readonly birthDate: Date;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressStreet: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressZipCode: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressDistrict: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressCity: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly addressState: string;
+
+  @IsOptional()
+  @IsString()
   readonly addressComplement: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   readonly active: boolean;
 }
