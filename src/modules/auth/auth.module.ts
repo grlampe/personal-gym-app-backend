25483b5293fdab86afb28cd5a6e903ecfb2bfd9a
Gabriel Lampe <grlampe@hotmail.com>
2023-08-15 17:31:03 -0300

    feat: new time to expires jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '4h' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
